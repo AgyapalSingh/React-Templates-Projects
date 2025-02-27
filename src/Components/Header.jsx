5825db0e66c19b8e074b465c6fa85b3c70e9534f
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-27 16:52:51 +0530

    Header Updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="Header">
-      <h1>Developed By -
-        <a
-          href="https://www.linkedin.com/in/agyapal-singh-020983241/"
-          target="_blank"
-        >
-          Agyapal Singh
-        </a></h1>
-    </div>
+        <h1>
+          Developed By -
+          <a
+            href="https://www.linkedin.com/in/agyapal-singh-020983241/"
+            target="_blank"
+          >
+            Agyapal Singh
+          </a>
+        </h1>
+      </div>
+      <div className="home-link">
+        <Link className="home-link" to="/">Home</Link>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
