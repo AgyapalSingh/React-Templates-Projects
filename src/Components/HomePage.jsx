b93907192b12162a0540d4c0edd81d9c62d60591
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-27 17:47:40 +0530

    Cards added in scroll for Mobile

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -6,37 +6,37 @@ import data from "../Data/Data.json";
 const HomePage = () => {
   return (
     <main className="ag-tempalte-container">
-      <h1 className="ag-template-title">
-        List of Templates
-      </h1>
+      <h1 className="ag-template-title">List of Templates</h1>
 
       <section className="ag-template-section">
-        {data.map((item, index) => (
-          <div className="ag-template-card" key={index}>
-            <div className="ag-template-card-image">
-              {item.img && <img src={item.img} alt={item.title} />}
-            </div>
-
-            <div className="ag-template-card-content">
-              <div className="ag-template-card-info">
-                <h1>{item.title}</h1>
-                <span>
-                  Contact Form is linked to
-                  <i>
-                    {" "}
-                    <b>{item["description-2"]}</b>
-                  </i>
-                </span>
+        <div className="ag-template-card-container">
+          {data.map((item, index) => (
+            <div className="ag-template-card" key={index}>
+              <div className="ag-template-card-image">
+                {item.img && <img src={item.img} alt={item.title} />}
               </div>
-            </div>
 
-            <div className="ag-template-card-btn">
+              <div className="ag-template-card-content">
+                <div className="ag-template-card-info">
+                  <h1>{item.title}</h1>
+                  <span>
+                    Contact Form is linked to
+                    <i>
+                      {" "}
+                      <b>{item["description-2"]}</b>
+                    </i>
+                  </span>
+                </div>
+              </div>
+
+              <div className="ag-template-card-btn">
                 <Link to={`/${item.linkTo}`}>
                   <p>View Template</p>
                 </Link>
               </div>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </section>
       {/* <section className="ag-template-section">
         <div className="ag-template-card">
