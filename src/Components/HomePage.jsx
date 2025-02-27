4da7a89ca33adfb99f536b4b6360206ffc33b46e
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-27 16:33:15 +0530

    Dynamic Cards Created

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import data from "../Data/Data.json";
 
 const HomePage = () => {
   return (
@@ -9,17 +10,50 @@ const HomePage = () => {
         <FaBars />
         List of Templates
       </h1>
+
       <section className="ag-template-section">
+        {data.map((item, index) => (
+          <div className="ag-template-card" key={index}>
+            <div className="ag-template-card-image">
+              {item.img && <img src={item.img} alt={item.title} />}
+            </div>
+
+            <div className="ag-template-card-content">
+              <div className="ag-template-card-info">
+                <h1>{item.title}</h1>
+                <span>
+                  Contact Form is linked to
+                  <i>
+                    {" "}
+                    <b>{item["description-2"]}</b>
+                  </i>
+                </span>
+              </div>
+
+              <div className="ag-template-card-btn">
+                <Link to={`/${item.linkTo}`}>
+                  <p>View Template</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+      {/* <section className="ag-template-section">
         <div className="ag-template-card">
           <div className="ag-template-card-image">
-            <img src="" alt="img" />
+            <img src={IMG1} alt="img" />
           </div>
 
           <div className="ag-template-card-content">
             <div className="ag-template-card-info">
-              <h1>Affiliate Program Page</h1>
+              <h1>{data.title}</h1>
               <span>
-                Contact Form is linked to<i> <b> Google Sheet using API. </b></i>
+                Contact Form is linked to
+                <i>
+                  {" "}
+                  <b> Google Sheet using API. </b>
+                </i>
               </span>
             </div>
 
@@ -30,7 +64,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   );
 };
