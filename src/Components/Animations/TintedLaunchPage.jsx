86d95f2c863805381bddce1b4f76975d83a2363b
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-01 17:44:00 +0530

    Animation on Tittle Added in TS Launch Page

--- src/Components/Animations/TintedLaunchPage.jsx
+++ src/Components/Animations/TintedLaunchPage.jsx
@@ -1,27 +1,106 @@
-import React from "react";
-import "./AnimationCSS/TintedLaunchPage.css"
+import React, { useEffect } from "react";
+import "./AnimationCSS/TintedLaunchPage.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const TintedLaunchPage = () => {
+
+  useEffect(() => {
+    let tl = gsap.timeline();
+   
+    tl.to(".title-part-Hook-1", {
+      opacity: 0,
+      scale: 1.5,
+      scrollTrigger: {
+        trigger: ".ag-launch-page-title",
+        start: "top 5%",
+        end: "top 0%",
+        scrub: 1,
+      },
+    })
+      .to(".title-part-Hook-2", {
+        opacity: 1,
+        scale: 1.5,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top 0%",
+          end: "top -15%",
+          scrub: 1,
+        },
+      })
+      .to(".title-part-Hook-2", {
+        opacity: 0,
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top -15%",
+          end: "top -20%",
+          scrub: 1,
+        },
+      })
+      .to(".title-part-Hook-3", {
+        opacity: 1,
+        scale: 1.5,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top -20%",
+          end: "top -35%",
+          scrub: 1,
+        },
+      })
+      .to(".title-part-Hook-3", {
+        opacity: 0,
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top -35%",
+          end: "top -40%",
+          scrub: 1,
+        },
+      })
+      .to(".title-part-Hook-4", {
+        opacity: 1,
+        scale: 1.5,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top -40%",
+          end: "top -55%",
+          scrub: 1,
+        },
+      })
+      .to(".title-part-Hook-4", {
+        opacity: 0,
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".ag-launch-page-title",
+          start: "top -55%",
+          end: "top -60%",
+          scrub: 1,
+        },
+      });
+  }, []);
+
   return (
     <main className="ag-tempalte-container">
       <section className="ag-launch-page-container">
         <div className="ag-launch-page-title">
-          <div className="ag-launch-title-part">
+          <div className="ag-launch-title-part title-part-Hook-1">
             <h1>Tinted</h1>
           </div>
 
-          <div className="ag-launch-title-part" >
+          <div className="ag-launch-title-part title-part-Hook-2">
             <h1>Just</h1>
           </div>
 
-          <div className="ag-launch-title-part">
+          <div className="ag-launch-title-part title-part-Hook-3">
             <h1>Got</h1>
           </div>
 
-          <div className="ag-launch-title-part">
+          <div className="ag-launch-title-part title-part-Hook-4">
             <h1>Better</h1>
           </div>
-
         </div>
       </section>
     </main>
