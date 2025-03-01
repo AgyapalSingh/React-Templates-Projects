c8c1cefca5890fb7ec6c74d995d2010e6eec5feb
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-01 17:30:47 +0530

    TintedLaunchPAge.jsx component edited

--- src/Components/Animations/TintedLaunchPage.jsx
+++ src/Components/Animations/TintedLaunchPage.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import "./AnimationCSS/TintedLaunchPage.css"
 
 const TintedLaunchPage = () => {
   return (
     <main className="ag-tempalte-container">
-      <div>TintedLaunchPage</div>
+      <section className="ag-launch-page-container">
+        <div className="ag-launch-page-title">
+          <div className="ag-launch-title-part">
+            <h1>Tinted</h1>
+          </div>
+
+          <div className="ag-launch-title-part" >
+            <h1>Just</h1>
+          </div>
+
+          <div className="ag-launch-title-part">
+            <h1>Got</h1>
+          </div>
+
+          <div className="ag-launch-title-part">
+            <h1>Better</h1>
+          </div>
+
+        </div>
+      </section>
     </main>
   );
 };
