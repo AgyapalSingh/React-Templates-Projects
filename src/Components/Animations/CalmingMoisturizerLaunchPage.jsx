3f94f61b5401a74aa5d45c799480a40e4ac866a2
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-11 15:23:22 +0530

    Pre_order Animation trigger updated

--- src/Components/Animations/CalmingMoisturizerLaunchPage.jsx
+++ src/Components/Animations/CalmingMoisturizerLaunchPage.jsx
@@ -102,9 +102,9 @@ const CalmingMoisturizerLaunchPage = () => {
       duration: 0.5,
       opacity: 0,
       scrollTrigger: {
-        trigger: ".ag-product-animation-container-m-1-cm",
-        start: "top 75%",
-        end: "top 45%",
+        trigger: ".ag-product-benefit-container-cm",
+        start: "top 100%",
+        end: "top 75%",
         scrub: 1,
         // markers: true,
       },
