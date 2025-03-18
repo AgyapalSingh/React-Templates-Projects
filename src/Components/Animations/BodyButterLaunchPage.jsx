db60fbc9d2f9145cedee50916fac77411eabe36c
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-18 17:34:07 +0530

    CSS Updated and More sections Added to BB_LP

--- src/Components/Animations/BodyButterLaunchPage.jsx
+++ src/Components/Animations/BodyButterLaunchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import "./AnimationCSS/BodyButterLaunchPage.css";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { Link } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -202,7 +203,130 @@ const BodyButterLaunchPage = () => {
           </ul>
         </div>
 
-        <div className="em"></div>
+        {/* Product Ingredients */}
+        <div className="ag-animation-bb">
+          <h1>Because We Used :</h1>
+        </div>
+
+        <div className="ag-product-ingredients-container-bb">
+          <div className="ag-product-ingredients-container-div-bb">
+            <div className="ag-product-ingredient-card-bb">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Italian-Butter-Ing.png?v=1735884931"
+                loading="lazy"
+                alt="Italian Cocoa Butter"
+              />
+              <h1>Italian Cocoa Butter</h1>
+              <span className="ingredient-info-bb">
+                Deeply hydrates and improves skin elasticity.
+              </span>
+            </div>
+            <div className="ag-product-ingredient-card-bb">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/BB-I-Shea-Butter.png?v=1735814508"
+                loading="lazy"
+                alt="Shea Butter"
+              />
+              <h1>Shea Butter</h1>
+              <span className="ingredient-info-bb">
+                Nourishes and softens dry, rough skin.
+              </span>
+            </div>
+            <div className="ag-product-ingredient-card-bb">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Coffee-Ing.png?v=1735883615"
+                loading="lazy"
+                alt="Coffee Arabica Seed"
+              />
+              <h1>Coffee Arabica Seed</h1>
+              <span className="ingredient-info-bb">
+                Tones and revitalizes skin.
+              </span>
+            </div>
+            <div className="ag-product-ingredient-card-bb">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Senegal-Gum.png?v=1735880966"
+                loading="lazy"
+                alt="Acacia Senegal Gum"
+              />
+              <h1>Acacia Senegal Gum</h1>
+              <span className="ingredient-info-bb">
+                Enhances skin hydration and resilience.
+              </span>
+            </div>
+            <div className="ag-product-ingredient-card-bb">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Hyaluronic_Acid_4fec6bd7-f837-4620-954f-f0b805d33593.png?v=1735880965"
+                loading="lazy"
+                alt="Hyaluronic Acid"
+              />
+              <h1>Hyaluronic Acid</h1>
+              <span className="ingredient-info-bb">
+                Locks in moisture for soft, glowing skin.
+              </span>
+            </div>
+          </div>
+        </div>
+
+        {/* Product Results */}
+        <div className="ag-animation-bb">
+          <h1>Acne to AcNO In 3 Weeks :</h1>
+        </div>
+
+        <div className="ag-product-result-bb">
+          <video className="img-small-bb" autoPlay loop muted playsInline>
+            <source src="https://cdn.shopify.com/videos/c/o/v/7f9a6b6e316a4cdb85b6cfe2117c4ec6.mp4" />
+            <source src="movie.ogg" type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
+          <video className="img-large-bb" autoPlay loop muted playsInline>
+            <source src="https://cdn.shopify.com/videos/c/o/v/f16c9fd36a1c4691a07303353fe7026e.mp4" />
+            <source src="movie.ogg" type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
+        <div className="ag-animation-bb">
+          <h1>"Trusted by them, perfect for you."</h1>
+        </div>
+
+        <div className="ag-products-reels-bb">
+          <div className="ag-product-reel-bb">
+            <p>Reel 1</p>
+          </div>
+
+          <div className="ag-product-reel-bb">
+            <p>Reel 2</p>
+          </div>
+
+          <div className="ag-product-reel-bb">
+            <p>Reel 3</p>
+          </div>
+
+          <div className="ag-product-reel-bb">
+            <p>Reel 4</p>
+          </div>
+        </div>
+
+        <div className="ag-pre-order-btn-bottm-div-bb">
+          <Link className="ag-pre-order-btn-bottm-bb">Pre-Order</Link>
+        </div>
+
+        <div className="ag-pre-order-btn-div-bb">
+          <Link className="ag-pre-order-btn-bb">Pre-Order</Link>
+        </div>
+
+        <div className="ag-scroll-text">
+          <p>
+            <span>S</span>
+            <span>C</span>
+            <span>R</span>
+            <span>O</span>
+            <span>L</span>
+            <span>L</span>
+            <span>↓</span>
+          </p>
+        </div>
       </section>
     </main>
   );
