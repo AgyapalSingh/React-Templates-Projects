3cb8197bd8f001d143a443003d87841686de2122
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-18 14:53:16 +0530

    Body Butter Title Setup

--- src/Components/Animations/BodyButterLaunchPage.jsx
+++ src/Components/Animations/BodyButterLaunchPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./AnimationCSS/BodyButterLaunchPage.css"
 
 const BodyButterLaunchPage = () => {
   return (
     <main className="ag-tempalte-container">
-      <div>BodyButterLaunchPage</div>
+      <section className="ag-launch-page-container-bb">
+        <div className="bb-product-title">
+          <h1 id="bb-animated-title">A.S.M.B.B</h1>
+        </div>
+      </section>
     </main>
   );
 };
